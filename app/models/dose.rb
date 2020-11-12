63e8a86860a3fd406774de85bf6e70009e48da24
hadrien le bret <hadrien.lebret@gmail.com>
2020-11-12 12:33:10 +0100

    associations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,9 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, presence: true
+  # validates :cocktail_id && :ingredient_id, uniqueness: true
+
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
