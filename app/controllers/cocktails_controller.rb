dbfd4cfc895f1c64291bdc0e57e9b9359a9119c0
hadrien le bret <hadrien.lebret@gmail.com>
2020-11-12 16:58:11 +0100

    exo

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,9 +15,11 @@ class CocktailsController < ApplicationController
   def create
     # raise
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
-
-    redirect_to cocktail_path(@cocktail)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   private # strong params
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,23 +1,24 @@
 class DosesController < ApplicationController
+
   def new
     @dose = Dose.new
   end
 
   def create
-    # raise
-    @dose = Dose.new(new_dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
+
     if @dose.save
-      redirect_to new_dose_path(@dose)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
   end
 
   def destroy
-    @dose = Review.find(params[:id])
+    @dose = Dose.find(params[:id])
     @dose.destroy
-
     redirect_to cocktail_path(@dose.cocktail)
   end
 
@@ -26,4 +27,5 @@ class DosesController < ApplicationController
   def dose_params
     params.require(:dose).permit(:description)
   end
+
 end
