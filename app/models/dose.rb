893beb9adbf66d0a5f64979e9aba345fdddfc1b5
hadrien le bret <hadrien.lebret@gmail.com>
2020-11-12 13:06:22 +0100

    json

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,7 +3,7 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
-  # validates :cocktail_id && :ingredient_id, uniqueness: true
 
-  validates :cocktail, uniqueness: { scope: :ingredient }
+  # validates :cocktail_id && :ingredient_id, uniqueness: true
+  validates :cocktail, uniqueness: { scope: :ingredient } # is unique for a given cocktail/ingredient couple
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+require "open-uri"
+require "json"
+
+puts "Deleting all records"
+Ingredient.delete_all
+
+puts "Seeding all ingredients from JSON file"
+response = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+data = JSON.parse(response)
+data["drinks"].each do |item|
+  ingredient = Ingredient.new(
+    name: item["strIngredient1"]
+  )
+  ingredient.save!
+end
+puts "All done :)"
